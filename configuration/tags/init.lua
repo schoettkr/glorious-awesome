d1fb9d8ade8799dbc655f593286ee78600602b50
LS8 <lennartschoettker@hotmail.com>
2020-08-07 14:27:04 +0200

    Change default screens, orientations, apps

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -1,24 +1,25 @@
 local awful = require('awful')
 local gears = require('gears')
 local icons = require('theme.icons')
+local apps = require('configuration.apps')
 
 local tags = {
   {
-    icon = icons.terminal,
-    type = 'terminal',
-    defaultApp = 'kitty',
-    screen = 1
-  },
-  {
     icon = icons.chrome,
     type = 'chrome',
-    defaultApp = 'firefox',
+    defaultApp = apps.default.browser,
     screen = 1
   },
   {
-    icon = icons.code,
+    icon = icons.emacs,
     type = 'code',
-    defaultApp = 'atom',
+    defaultApp = apps.default.editor,
+    screen = 1
+  },
+  {
+    icon = icons.terminal,
+    type = 'terminal',
+    defaultApp = apps.default.terminal,
     screen = 1
   },
  --[[ {
@@ -30,56 +31,84 @@ local tags = {
   {
     icon = icons.folder,
     type = 'files',
-    defaultApp = 'nemo',
+    defaultApp = apps.default.fileManager,
     screen = 1
   },
   {
-    icon = icons.music,
-    type = 'music',
-    defaultApp = 'kitty -e ncmpcpp',
-    screen = 1
-  },
-  {
-    icon = icons.game,
-    type = 'game',
-    defaultApp = 'supertuxkart',
-    screen = 1
-  },
-  {
-    icon = icons.art,
-    type = 'art',
-    defaultApp = 'gimp',
-    screen = 1
-  },
-  {
-    icon = icons.vbox,
-    type = 'virtualbox',
-    defaultApp = 'virtualbox',
-    screen = 1
-  },
-  {
-    icon = icons.lab,
+    icon = icons.any,
     type = 'any',
-    defaultApp = '',
+    -- defaultApp = apps.default.fileManager,
+    screen = 1
+  },
+  {
+    icon = icons.labplot,
+    type = 'monitor',
+    defaultApp = apps.default.monitor,
     screen = 1
   }
+  -- {
+  --   icon = icons.music,
+  --   type = 'music',
+  --   defaultApp = 'kitty -e ncmpcpp',
+  --   screen = 1
+  -- },
+  -- {
+  --   icon = icons.game,
+  --   type = 'game',
+  --   defaultApp = 'supertuxkart',
+  --   screen = 1
+  -- },
+  -- {
+  --   icon = icons.art,
+  --   type = 'art',
+  --   defaultApp = 'gimp',
+  --   screen = 1
+  -- },
+  -- {
+  --   icon = icons.vbox,
+  --   type = 'virtualbox',
+  --   defaultApp = 'virtualbox',
+  --   screen = 1
+  -- },
+  -- {
+  --   icon = icons.lab,
+  --   type = 'any',
+  --   defaultApp = '',
+  --   screen = 1
+  -- }
 }
 
 awful.layout.layouts = {
-  awful.layout.suit.spiral.dwindle,
+  -- awful.layout.suit.spiral.dwindle,
   awful.layout.suit.tile,
-  awful.layout.suit.max
+  awful.layout.suit.max,
+  awful.layout.suit.floating,
 }
 
 
 screen.connect_signal("request::desktop_decoration", function(s)
   for i, tag in pairs(tags) do
+    -- if (i == 3) then
+    -- awful.tag.add(
+    --   i,
+    --   {
+    --     icon = tag.icon,
+    --     icon_only = true,
+    --     layout = awful.layout.suit.floating,
+    --     gap_single_client = false,
+    --     gap = 4,
+    --     screen = s,
+    --     defaultApp = tag.defaultApp,
+    --     selected = i == 1
+    --   }
+    -- )
+    -- else
     awful.tag.add(
       i,
       {
         icon = tag.icon,
         icon_only = true,
-        layout = awful.layout.suit.spiral.dwindle,
+        layout = awful.layout.suit.tile,
         gap_single_client = false,
         gap = 4,
         screen = s,
@@ -87,6 +116,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
         selected = i == 1
       }
     )
+    -- end
   end
 end)
 
